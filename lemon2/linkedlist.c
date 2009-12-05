47cc33318daa868f3905ae6fb1af8b149a9ee3a5
kook <kook@kook-laptop.(none)>
2009-12-05 17:47:35 +0100

    fixed ctrl - d

---
+++ lemon2/linkedlist.c
@@ -0,0 +1,19 @@
+TYPE* rotefacelistremove(TYPE * first, TYPE * item)
+{
+    if(item==first)
+	return first->next;
+    else
+    {
+	TYPE * next = first;
+	while(next)
+	{
+	    if(next->next==item)
+	    {
+	        next->next=item->next;
+	        return first;
+	    }
+	    next=next->next;
+	}
+    }
+    return 0;
+}
