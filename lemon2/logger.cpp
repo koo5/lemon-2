7b0eb8a9b86f58ef02f0f359c06624fb2ad97b9d
koom <you@example.com>
2010-07-05 17:40:15 +0200

    adding logger, ifed atlantis

---
+++ lemon2/logger.cpp
@@ -0,0 +1,27 @@
+class logger:public face
+{
+    public:
+    logger(double x, double y, double z, double a, double b, double c)
+    {
+	obj::t.x=x;obj::t.y=y;obj::t.z=z;
+	r.x=a;r.y=b;r.z=c;
+	init();
+	t = rote_vt_create(100,100);
+    }
+	
+	
+    ~logger(){
+	rote_vt_destroy(t);
+    }
+    
+    void logit(char *s)
+    {
+	rote_vt_write(t, s, strlen(s)+1);
+	rote_vt_write(t,"\n",1);
+    }
+    
+    void slogit(char *s)
+    {
+	updatestatus(s);
+    }
+};
